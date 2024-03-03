86174aa51a4ccd2de40a0561992b6843de58a4ed
Vikas David Jonathan <vikas.jonathan@gmail.com>
2024-03-02 23:17:52 -0600

    Added conditions for the choices for function

--- index.js
+++ index.js
@@ -19,11 +19,29 @@ const prompUser = () => {
             "Exit",
         ],
     },
-])
-
-.then(answers) => {
-    const {choices} =answers;
-    
-}
-
-}
+    ])
+    // added condition as per choice calling the respective functions
+        .then((answers) => {
+            const { choices } = answers;
+            console.log(choices);
+            if (choices === "View All Employees") {
+                viewAllEmployees();
+            } else if (choices === "Add Employee") {
+                addEmployee();
+            } else if (choices === "View All Roles") {
+                viewAllRoles();
+            } else if (choices === "Add Role") {
+                addRole();
+            } else if (choices === "View All Departments") {
+                viewAllDepartments();
+            } else if (choices === "Add Department") {
+                addDepartment();
+            } else if (choices === "Update Employee Role") {
+                updateEmployeeRole();
+            } else if (choices === "Exit") {
+                db.end();
+            } else {
+                console.log("No selection was matched");
+            }
+        });
+};
